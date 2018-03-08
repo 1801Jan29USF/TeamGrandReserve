e1cc430d26c1a47e34248f1c26a7b1362e9131c3
Adrian Deleon <adr.m.del@gmail.com>
2018-03-08 13:48:12 -0500

    better forms, better functionality, papa john's

--- client/GrandReserveAngular/src/app/components/pregame/pregame.component.ts
+++ client/GrandReserveAngular/src/app/components/pregame/pregame.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-pregame',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PregameComponent implements OnInit {
 
+  //this is all dummy values for testing
+  redTeam = [
+    {
+      name: "1",
+      leader: false
+
+    },
+    {
+      name: "2",
+      leader: false
+    },
+    {
+      name: "3",
+      leader: false
+    },
+    {
+      name: "4",
+      leader: true
+    }
+  ];
+  blueTeam = [
+    {
+      name: "b1",
+      leader: false
+    },
+    {
+      name: "b2",
+      leader: false
+    },
+    {
+      name: "b3",
+      leader: true
+    },
+    {
+      name: "b4",
+      leader: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setLeader(player){
+    if(!player.leader){
+      if(this.redTeam.includes(player)){
+        this.redTeam.forEach(function(value){
+          if(value.leader){
+            value.leader = false;
+          }
+        })
+      }
+      else if(this.blueTeam.includes(player)){
+        this.blueTeam.forEach(function(value){
+          if(value.leader){
+            value.leader = false;
+          }
+        })
+      }
+      player.leader = true;
+      alert('This player is now the leader')
+    }
+    else{
+      alert('This player is already the leader.');
+    }
   }
 
 }
