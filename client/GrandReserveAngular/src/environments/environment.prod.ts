997f02f8345fc147a7cabc5b430d020b39a87798
Adrian Deleon <adr.m.del@gmail.com>
2018-03-18 18:22:11 -0400

    production environmnet

--- client/GrandReserveAngular/src/environments/environment.prod.ts
+++ client/GrandReserveAngular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  context: 'http://ec2-18-217-91-70.us-east-2.compute.amazonaws.com:8090/server/',
+  ws: 'ws:ec2-18-217-91-70.us-east-2.compute.amazonaws.com:8090/server/'
 };
